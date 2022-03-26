0394376934c46e6f8c161a4798da56d195654949
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2022-03-26 16:41:53 -0400

    Implemented basic page renders.

--- src/components/PdfDocument.vue
+++ src/components/PdfDocument.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
-import { ref, watch, shallowRef, onUnmounted } from 'vue'
+import { ref, watch, shallowRef, onUnmounted, onMounted, onBeforeUnmount } from 'vue'
 import { GlobalWorkerOptions, getDocument, PDFDataRangeTransport, PDFDocumentProxy, PDFPageProxy } from 'pdfjs-dist'
-import { TypedArray, DocumentInitParameters } from 'pdfjs-dist/types/src/display/api'
+// import { TypedArray, DocumentInitParameters } from 'pdfjs-dist/types/src/display/api'
 import PdfPage from './PdfPage.vue'
 
-const props = defineProps<{ workerJs: string, src: string | URL | TypedArray | PDFDataRangeTransport | DocumentInitParameters }>()
+const props = defineProps<{ workerJs: string, src: string | URL | PDFDataRangeTransport }>()// | TypedArray | DocumentInitParameters }>()
 // defineEmits)
 
 const error = ref('')
 const pdfDoc = shallowRef<PDFDocumentProxy | null>(null)
 const pdfPages = shallowRef<PDFPageProxy[]>([])
+const zoom = ref(1)
+
 function cleanupDoc() {
   pdfPages.value.forEach(pg => pg.cleanup())
   pdfPages.value = []
@@ -52,12 +54,38 @@ watch(pdfDoc, doc => {
   })
 })
 
+const pixelRatio = ref(window.devicePixelRatio)
+function updatePixelRatio() {
+  // console.log('current pixel ratio=', window.devicePixelRatio)
+  pixelRatio.value = window.devicePixelRatio
+}
+
+// listen for dpi change
+const mediaQuery = matchMedia(`(resolution: 1dppx)`)
+
+onMounted(() => {
+  mediaQuery.addEventListener("change", updatePixelRatio)
+})
+onBeforeUnmount(() => {
+  mediaQuery.removeEventListener("change", updatePixelRatio)
+})
+
 </script>
 
 <template>
   <div>
     <p>{{ src }}</p>
     <p v-if="error">{{ error }}</p>
-    <pdf-page v-for="page in pdfPages" :key="page.pageNumber" :page="page"></pdf-page>
+    <pdf-page
+      v-for="page in pdfPages"
+      :key="page.pageNumber"
+      :page="page"
+      :pixel-ratio="pixelRatio"
+      :zoom="zoom"
+    >
+      <template #default="{ width, height }">
+        <slot :page="page.pageNumber" :width="width" :height="height"></slot>
+      </template>
+    </pdf-page>
   </div>
 </template> 
--- src/components/PdfPage.vue
+++ src/components/PdfPage.vue
@@ -1,10 +1,75 @@
 <script setup lang="ts">
-import { PDFPageProxy } from 'pdfjs-dist'
-import { shallowRef } from 'vue'
+import { computed } from '@vue/reactivity'
+import { PageViewport, PDFPageProxy } from 'pdfjs-dist'
+import { onMounted, shallowRef, watch, ref } from 'vue'
 
-const props = defineProps<{ page: PDFPageProxy }>()
+const props = defineProps<{ zoom: number, pixelRatio: number, page: PDFPageProxy }>()
+const canvas = ref<HTMLCanvasElement | undefined>()
+
+const pdfScale = computed(() => {
+  return props.zoom * (96 / 72)
+})
+const displayScale = computed(() => {
+  return pdfScale.value * (props.pixelRatio || 1)
+})
+const canvasAttrs = computed(() => {
+  const vp = props.page.getViewport({ scale: pdfScale.value })
+
+  return {
+    width: vp.width * displayScale.value,
+    height: vp.height * displayScale.value,
+    style: {
+      width: vp.width,
+      height: vp.height
+    }
+  }
+})
+
+function renderPage() {
+  if (!props.page || !canvas.value) return
+
+  const page = props.page
+
+  console.log(`rendering page ${page.pageNumber}`)
+
+  const sizes = canvasAttrs.value
+  canvas.value.width = sizes.width
+  canvas.value.height = sizes.height
+  canvas.value.style.width = sizes.style.width + 'px'
+  canvas.value.style.height = sizes.style.height + 'px'
+
+  const context = canvas.value.getContext('2d')
+  if (!context) return
+
+  const renderContext = {
+    canvasContext: context,
+    transform: [displayScale.value, 0, 0, displayScale.value, 0, 0],
+    viewport: page.getViewport({ scale: displayScale.value })
+  }
+  page.render(renderContext)
+}
+
+// defineExpose({
+//   renderPage
+// })
+
+watch(() => [props.page, displayScale.value], () => {
+  renderPage()
+}, { immediate: true })
+
+onMounted(() => {
+  renderPage()
+})
 
 </script>
 <template>
-  <div>{{ page.pageNumber }}</div>
+  <div style="margin-bottom:1rem; display:flex; flex-direction: column;">
+    <div
+      style="position:relative; display:flex; user-select: none; outline: 1px solid #ddd; margin-bottom:.5rem; margin:auto;"
+    >
+      <canvas ref="canvas"></canvas>
+      <slot :width="canvasAttrs.width" :height="canvasAttrs.height"></slot>
+    </div>
+    <div style="text-align:center; line-height: 1.5;">{{ page.pageNumber }}</div>
+  </div>
 </template>

a262e46f3aabaf6c5d48ddb5f8f32327c14f1302
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2022-08-21 06:04:46 -0400

    Made lib build separate for netlify preview.

---
+++ vite.config.lib.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+const path = require('path')
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'pdfjsVue3',
+      fileName: format => `pdfjs-vue3.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue', 'pdfjs-dist'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+          'pdfjs-dist': 'pdfjsLib'
+        }
+      }
+    }
+  }
+})
